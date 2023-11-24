702f551182ff2b0ed461218bfcb21d077fc007a3
Samira saud <samirasaudit@gmail.com>
2023-11-24 22:09:50 +0100

    Fatto

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Forma forma= new Forma(4,2);
+        Rettangolo rettangolo1= new Rettangolo(9,3);
+
+        forma.calcoloArea();
+        rettangolo1.calcoloArea();
+
+    }
+    }
---
+++ src/Rettangolo.java
@@ -0,0 +1,11 @@
+public class Rettangolo extends Forma {
+    public Rettangolo(Integer base, Integer altezza) {
+        super(base, altezza);
+    }
+
+    public void calcoloArea() {
+        int calcoloAreaRettangolo = base * altezza;
+        System.out.println("Il Area dello Rettangolo è :" + calcoloAreaRettangolo);
+    }
+}
+
